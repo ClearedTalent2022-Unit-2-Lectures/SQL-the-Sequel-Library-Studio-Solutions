7400896fb60c6609675db4bc69138184ac7c7c24
Jim Flores <jflores@launchcode.org>
2020-12-03 18:36:40 +0000

    Update bonusmissions.sql

--- sql-studio-part2/bonusmissions.sql
+++ sql-studio-part2/bonusmissions.sql
@@ -2,6 +2,13 @@
 
 SELECT genre_id, COUNT(*) FROM book GROUP BY genre_id;
 
+-- Alternatively, return the counts of the books from each genre and include the name of the genre.
+
+SELECT book.genre_id, COUNT(*), genres
+FROM book
+INNER JOIN genre ON genre.genre_id = book.genre_id
+GROUP BY book.genre_id;
+
 # A reference book cannot leave the library. How would you modify either the reference_book table or the book table to make sure that doesn’t happen? Try to apply your modifications.
 
 UPDATE book
